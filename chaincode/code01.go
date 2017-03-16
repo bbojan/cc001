d52eac46707095b3bf69982f10f82fac68bb5cbd
BojanB <bojan.bozinovski@seavus.com>
2017-03-16 15:49:26 +0100

    unbreak

--- chaincode/code01.go
+++ chaincode/code01.go
@@ -16,7 +16,6 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-asdw34r23fvzcfv d dfgw45 y4 
 
 package main
 
