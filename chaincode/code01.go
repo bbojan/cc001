d57e6144755b6f1f4753aaf658f56842298dae76
BojanB <bojan.bozinovski@seavus.com>
2017-03-16 15:48:45 +0100

    break build

--- chaincode/code01.go
+++ chaincode/code01.go
@@ -16,6 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
+asdw34r23fvzcfv d dfgw45 y4 
 
 package main
 
